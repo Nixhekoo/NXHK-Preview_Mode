698a2377b379a1f7a6126117077a231d324d6d7b
Nixhekoo <160276153+Nixhekoo@users.noreply.github.com>
2024-06-03 12:18:06 +0200

    Delete data directory

--- data/NXHKTPcreatefolder.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-if not exist "%LOCALAPPDATA%\NXHKTP" (mkdir "%LOCALAPPDATA%\NXHKTP")
-if not exist "%LOCALAPPDATA%\NXHKTP\source" (mkdir "%LOCALAPPDATA%\NXHKTP\source")
-if not exist "%LOCALAPPDATA%\NXHKTP\dlc" (mkdir "%LOCALAPPDATA%\NXHKTP\dlc")
